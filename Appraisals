8bf2fa6d449a4d8ac368573cd2da72e81ba57121
James Adam <james@lazyatom.com>
2017-02-10 13:08:33 +0000

    Avoid temporary creation of preference with wrong value
    A problem with the previous Rails 5 fix is that it would be possible for
    a different process to read a preference after it's been initially saved
    but before it's value has actually been updated. We can fix this by not
    saving the preference before it's been given the right value.
    
    To do this efficiently, we can use the `find_or_initialize_by` method
    introduced in Rails 4. We can also take advantage of the attributes API
    to ensure that the preference value is always converted into db format,
    which opens us up to potentially even simpler refactorings; the
    `update_value!` method now is largely redudant.
    
    In order to to use `find_or_initialize_by`, we've had to drop support
    for Rails 3.

--- Appraisals
+++ Appraisals
@@ -1,7 +1,3 @@
-appraise "rails-3" do
-  gem "rails", "~> 3"
-end
-
 appraise "rails-4" do
   gem "rails", "~> 4"
 end
--- lib/user_preferences/api.rb
+++ lib/user_preferences/api.rb
@@ -42,11 +42,7 @@ module UserPreferences
     end
 
     def find_or_init_preference(name)
-      unless preference = saved_preferences.detect { |p| p.name == name }
-        preference = @scope.find_by_name(name) || @scope.build(name: name, category: @category)
-        preference.save
-      end
-      preference
+      @scope.find_or_initialize_by(name: name, category: @category)
     end
 
     def hash_setter(&block)
